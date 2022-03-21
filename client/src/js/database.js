7080a94572131eae906220f390ca493dc9c85138
Mitchell Routman <mitchell.routman@gmail.com>
2022-03-21 16:22:31 -0400

    resolve put/get db outstanding todos

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,25 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  // console.error('putDb not implemented');
+  const jdb = await openDB('jate', 1);
+  const transaction = jdb.transaction('jate', 'readwrite');
+  const store = transaction.objectStore('jate');
+  const req = store.put({id: 5, value: content});
+  const resolution = await req;
+  console.log(resolution)
+}
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const jdb = await openDB('jate', 1);
+  const transaction = jdb.transaction('jate', 'readonly');
+  const store = transaction.objectStore('jate');
+  const req = store.get(5);
+  const resolution = await req;
+
+  return resolution?.value;
+}
 
 initdb();
